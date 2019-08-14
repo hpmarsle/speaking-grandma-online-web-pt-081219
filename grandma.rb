e96a84adf412b745d2d9236cb6b560233aa52ccb
Mars Le <hp.mars.le@gmail.com>
2019-08-14 02:25:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(sentence)
-  if sentence == sentence.upcase
-    "NO, NOT SINCE 1938!"
+  if sentence == "I LOVE YOU GRANDMA"
+    "I LOVE YOU TOO PUMPKIN!"
   # elsif sentence  != sentence.upcase
     # "HUH?! SPEAK UP, SONNY!"
-  elsif sentence == "I LOVE YOU GRANDMA"
-    "I LOVE YOU TOO PUMPKIN!"
+  elsif sentence == sentence.upcase
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end 
